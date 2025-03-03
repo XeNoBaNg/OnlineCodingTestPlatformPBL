fd5e2ab00a79a20128b08a099f430a56c8fb298d
Abhinav Chavan <149501379+XeNoBaNg@users.noreply.github.com>
2025-03-04 01:47:21 +0530

    updated some stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React from "react"
+import { Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx"
+import LoginRegister from "./pages/LoginRegister.jsx"
+import StudentRegister from "./pages/Student/StudentRegister.jsx"
+import StudentLogin from "./pages/Student/StudentLogin.jsx"
+import AdminLogin from "./pages/Admin/AdminLogin.jsx"
+import TestPage from "./pages/Tests/TestPage.jsx"
+import Leaderboard from "./pages/LeaderBoard.jsx"
+import AboutPage from "./pages/AboutPage.jsx"
 
 function App() {
     return ( 
         <>
-            <h1 className="">Hello World</h1>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginRegister />} />
+                <Route path="/studentregister" element={<StudentRegister />} />
+                <Route path="/studentlogin" element={<StudentLogin />} />
+                <Route path="/adminlogin" element={<AdminLogin />} />
+                <Route path="/test" element={<TestPage />} />
+                <Route path="leaderboard" element={<Leaderboard />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
         </>
     )
 }
